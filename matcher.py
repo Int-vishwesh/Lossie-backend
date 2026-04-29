a64eb2893fbee0348186ec895ac9bfc871ff0e24
Vishwesh Aryan <jerryaryan123@gmail.com>
2026-04-30 00:30:01 +0530

    threshold correction and vector=none

--- matcher.py
+++ matcher.py
@@ -33,16 +33,15 @@ def save_item(title: str, description: str, category: str, file_bytes: bytes = N
         consistency_score = calculate_similarity(text_vector, image_vector)
         print(f"Bouncer Score: {consistency_score}")
         
-        if consistency_score < 0.30:
+        if consistency_score < 0.25: # 25% min threshold
             # reject upload entirely
             raise HTTPException(
                 status_code=400, 
                 detail="Quality Alert: Your photo does not clearly match your description. Please upload a clearer photo or leave the image empty!"
             )
-        # ----------------------------------
 
         # when passes the bouncer
-        image_url = upload_image(file_bytes, filename, content_type)
+        image_url = upload_image (file_bytes, filename, content_type)
         db_data["image_url"] = image_url
         db_data["image_embedding"] = image_vector
         
@@ -59,7 +58,7 @@ def find_best_matches(category: str, description: str, file_bytes: bytes = None)
     if file_bytes:
         search_image_vector = generate_image_embedding(file_bytes)
     else:
-        search_image_vector = [0.0] * 512 
+        search_image_vector = None 
 
     target_search_category = "found" if category.lower() == "lost" else "lost"
     
@@ -69,7 +68,7 @@ def find_best_matches(category: str, description: str, file_bytes: bytes = None)
         {
             "search_text_vector": search_text_vector,
             "search_image_vector": search_image_vector,
-            "match_threshold": 0.85,
+            "match_threshold": 0.85, # 85% min threshold
             "match_count": 5,        
             "search_category": target_search_category
         }
